201189fbe950611055318d5cf338c854010aac15
Mike Audi <mike@mytiki.com>
2023-06-10 20:47:14 -0500

    fix: make api-error a builder

--- src/api/api-error.ts
+++ src/api/api-error.ts
@@ -6,24 +6,54 @@
 import { Response } from '@cloudflare/workers-types';
 import { APPLICATION_JSON, HeaderBuilder } from './api-headers';
 import { ApiCors } from './api-cors';
+import { skipNulls } from './api-consts';
 
 export class ApiError {
-  id?: string;
-  message?: string;
-  detail?: string;
-  help?: string;
-  properties?: Map<string, string>;
+  private _id?: string;
+  private _message?: string;
+  private _detail?: string;
+  private _help?: string;
+  private _properties?: Map<string, string>;
 
-  constructor(id: string, message: string, detail: string, help: string, properties: Map<string, string>) {
-    this.id = id;
-    this.message = message;
-    this.detail = detail;
-    this.help = help;
-    this.properties = properties;
+  id(id: string): ApiError {
+    this._id = id;
+    return this;
   }
 
-  toResponse = (status: number, cors?: ApiCors): Response =>
-    new Response(JSON.stringify(this), {
+  message(message: string): ApiError {
+    this._message = message;
+    return this;
+  }
+
+  detail(detail: string): ApiError {
+    this._detail = detail;
+    return this;
+  }
+
+  help(help: string): ApiError {
+    this._help = help;
+    return this;
+  }
+
+  properties(properties: Map<string, string>): ApiError {
+    this._properties = properties;
+    return this;
+  }
+
+  json = () =>
+    JSON.stringify(
+      {
+        id: this._id,
+        message: this._message,
+        detail: this._detail,
+        help: this._help,
+        properties: this._properties,
+      },
+      skipNulls
+    );
+
+  response = (status: number, cors?: ApiCors): Response =>
+    new Response(this.json(), {
       status,
       headers: new HeaderBuilder().from(cors?.headers()).content(APPLICATION_JSON).build(),
     });
